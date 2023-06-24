fa1d4511346a400ddb8646e12bdd4197296a94cd
Kavyansh Barjattya <117171893+kavyanshbarjattya@users.noreply.github.com>
2023-06-24 20:16:49 +0530

    updated carmove

--- Push_It/Assets/Scripts/car_controller.cs
+++ Push_It/Assets/Scripts/car_controller.cs
@@ -1,27 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class car_controller : MonoBehaviour
 {
+    [SerializeField] Transform front_right_trans;
+    [SerializeField] Transform front_left_trans;
+    [Header("This is for car movement")]
     [SerializeField] private float acceleration;
+    [SerializeField] private float brake;
     [SerializeField] private float steeringAngle = 20f;
     [SerializeField] private float Drag = 0.90f;
     [SerializeField] private float MaxSpeed = 15f;
     [SerializeField] private float Traction = 1f;
     [SerializeField] private VariableJoystick joystick;
+    [SerializeField] private Vector3 rotate;
+    [SerializeField] private float angle;
 
-    private Vector3 MoveForce;
+    private Vector2 MoveForce;
     private bool isSteering = false;
+    private float current_acceleration;
+    private float current_brake;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
 
     void Update()
     {
         //This is for z direction (forward & backward)
-        MoveForce += transform.forward * acceleration * joystick.Vertical * Time.deltaTime;
-        transform.position += MoveForce * Time.deltaTime;
+        MoveForce += transform.forward * acceleration * joystick.Direction * Time.deltaTime;
+        Debug.Log(joystick.Direction);
+        //transform.position += MoveForce * Time.deltaTime;
 
         //This is for x direction (left & right)
-        //float steeringInput = Input.GetAxisRaw("Horizontal");
         transform.Rotate(Vector3.up * joystick.Horizontal * MoveForce.magnitude * steeringAngle * Time.deltaTime);
 
         // Drag
@@ -46,6 +61,11 @@ public class car_controller : MonoBehaviour
         Debug.DrawRay(transform.position, transform.forward * 3 ,Color.red);
         MoveForce = Vector3.Lerp(MoveForce.normalized,transform.forward,Traction * Time.deltaTime) * MoveForce.magnitude;
     }
+    /*private void FixedUpdate()
+    {
+        angle = Mathf.Atan(joystick.Vertical * joystick.Horizontal);
+    }*/
+
     private void OnCollisionExit(Collision collision)
     {
         Debug.Log("Out");
---
+++ Push_It/Assets/Scripts/car_move.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car_move : MonoBehaviour
+{
+    [SerializeField] private VariableJoystick joystick;
+    [SerializeField] private Vector3 move;
+    [SerializeField] private float forward_speed;
+
+    private Rigidbody car_rb;
+    void Awake()
+    {
+        car_rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (joystick.Direction == Vector2.zero)
+            return;
+        move.x = joystick.Horizontal;
+        move.y = joystick.Vertical;
+        float yTrans = Mathf.Atan2(move.x, move.y) * Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0, yTrans - 90, 0);
+
+    }
+    private void FixedUpdate()
+    {
+        if (joystick.Direction != Vector2.zero)
+        {
+            car_rb.AddForce(transform.forward * forward_speed * Time.deltaTime);
+        }
+    }
+}
+    
