276f10c250b5faf062b612590e9e73e76cb14269
Kavyansh Barjattya <117171893+kavyanshbarjattya@users.noreply.github.com>
2023-06-25 19:25:10 +0530

    main_menu_updated

--- Push_It/Assets/Scripts/main_menu.cs
+++ Push_It/Assets/Scripts/main_menu.cs
@@ -7,13 +7,14 @@ public class main_menu : MonoBehaviour
 {
     [SerializeField] private Transform cam;
     [SerializeField] private float rot_speed;
-    [SerializeField] private GameObject main_menu1, main_menu2;
+    [SerializeField] private GameObject main_menu1, main_menu2, about_section;
     [SerializeField] private float wait_time = 0.5f;
     // Update is called once per frame
 
     private void Start()
     {
         main_menu2.SetActive(false);
+        about_section.SetActive(false);
     }
     void Update()
     {
@@ -28,6 +29,21 @@ public class main_menu : MonoBehaviour
         yield return new WaitForSeconds(wait_time);
         main_menu2.SetActive(true);
         main_menu1.SetActive(false);
+    }
+    public void About_btn()
+    {
+        StartCoroutine(About_btn_Timer());
+    }
+    IEnumerator About_btn_Timer()
+    {
+        yield return new WaitForSeconds(wait_time);
+        main_menu1.SetActive(false);
+        about_section.SetActive(true);
+    }
+    public void Back_btn()
+    {
+        about_section.SetActive(false);
+        main_menu1.SetActive(true); 
     }
     public void Quit_btn()
     {
